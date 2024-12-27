be009b7ee45726ca126f99bd4d41346bcbfe7744
swarno-coder <swarnodipnag2004@gmail.com>
2024-12-27 15:01:52 +0530

    Some changes

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import pandas as pd
-import joblib  
+import joblib, os
 
 @st.cache_data
 def load_model():
-    return [joblib.load(r'models\\equipment_model.pkl'),
-            joblib.load(r'models\\equipment_model2.pkl'),
-            joblib.load(r'models\\equipment_model3.pkl'),
-            joblib.load(r'models\\equipment_model4.pkl')]  # Load your model file
+    return [joblib.load(os.path.join(['models','equipment_model.pkl'])),
+            joblib.load(os.path.join(['models','equipment_model2.pkl'])),
+            joblib.load(os.path.join(['models','equipment_model3.pkl'])),
+            joblib.load(os.path.join(['models','equipment_model4.pkl']))]  # Load your model file
 
 model = load_model()
 
